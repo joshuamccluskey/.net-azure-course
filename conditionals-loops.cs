299699c2ae66bdfbbbb4ab9fb8a9e16b08b809e1
Joshua McCluskey <jpiff57@gmail.com>
2021-11-20 14:06:03 -0800

    Azure course code

---
+++ conditionals-loops.cs
@@ -0,0 +1,118 @@
+﻿//Name: Joshua McCluskey
+//Date: 11.20.2021
+//File Name: Program.cs
+//Purpose: Utilizing different forms of conditional statements and loops
+
+using System;
+
+namespace conditionals_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //if conditional statement
+            int grade = 98;
+            if (grade > 90)
+            {
+                Console.WriteLine("You got an A!");
+            }
+
+            //if else conditional statement
+            int grade1 = 80;
+            if (grade1 < 90)
+            {
+                Console.WriteLine("You got an B!");
+            }
+            else
+            {
+                Console.WriteLine("You got an A!");
+            }
+
+            // ternary operator contional short hand statement
+
+            int grade2 = 80;
+            var result = (grade2 > 90) ? "You got an A!" : "You got a B!";
+            Console.WriteLine(result);
+
+            //ternary operator extreme shorthand conditional statement
+            int grade3 = 90;
+            Console.WriteLine((grade3 < 90) ? "You got a B!" : "You got an A!");
+
+            //if else if conditional statement
+
+            int grade4 = 58;
+
+            if (grade4 < 60)
+            {
+                Console.WriteLine("You got an F!");
+            }
+            else if (grade4 < 70)
+            {
+                Console.WriteLine("You got a D!");
+            }
+            else if (grade4 < 80)
+            {
+                Console.WriteLine("You got a C!");
+            }
+            else if (grade4 < 90)
+            {
+                Console.WriteLine("You got a B!");
+            }
+            else
+            {
+                Console.WriteLine("You got an A!");
+            }
+
+            ////while loop counter
+            int counter = 0;
+            while (counter < 10)
+            {
+                Console.Write(counter);
+                counter++;
+            }
+
+            //do ...while loop counter
+            int counter1 = 3;
+            do
+            {
+                Console.Write(counter1);
+                counter1 --;
+            }
+            while (counter1 > 0);
+
+            //for loop counter
+
+            for (int i = 0; i < 10; i +=2)
+            {
+                Console.WriteLine(i);
+            }
+
+            //Combine for loop with if...statement
+            for (int i = 0; i < 100; i +=10)
+            {
+                if (i < 60)
+                {
+                    Console.WriteLine("You got an F!");
+                }
+                else if (i < 70)
+                {
+                    Console.WriteLine("You got a D!");
+                }
+                else if (i < 80)
+                {
+                    Console.WriteLine("You got a C!");
+                }
+                else if (i < 90)
+                {
+                    Console.WriteLine("You got a B!");
+                }
+                else
+                {
+                    Console.WriteLine("You got an A!");
+                }
+            }
+
+        }
+    }
+}
---
+++ hello-world.cs
@@ -0,0 +1,33 @@
+﻿//Name: Joshua McCluskey
+//Date: 11.17.2021
+//File Name: Program.cs
+//Purpose: This method is a HelloWorld app to print diffrent data types and variables to the consoles.
+
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // HelloWorld app to print diffrent data types to the consoles. This is my Hello World application and introduction to C# and Visual Studio
+            /*I'm introducing myself to the world using C#.
+             * Let me know if there is anything that I can improve
+             */
+            string FirstName = "Joshua";
+            string LastName = "McCluskey";
+            int Age = 30;
+            double Os = 12.01;
+            char MiddleInitial = 'C';
+            string City = "Seattle, WA";
+            bool Alive = true;
+            Console.WriteLine("Hello World! Hello I'm " + FirstName + " "+ MiddleInitial+ " " +LastName + "!");
+            Console.WriteLine("I'm " + Age);
+            Console.WriteLine("I live in " + City);
+            Console.WriteLine("I'm using macOS Monterey version " + Os);
+            Console.WriteLine("Am I alive? " + Alive);
+            Console.ReadKey();
+        }
+    }
+}
